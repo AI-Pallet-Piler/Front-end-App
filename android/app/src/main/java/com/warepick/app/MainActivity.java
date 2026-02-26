bd60f5da52d8ebd710be124e0ba2fdfad16e72ca
dominium2 <danial.mubeasher@student.ehb.be>
2026-02-26 14:57:52 +0100

    Refactor code structure for improved readability and maintainability

--- android/app/src/main/java/com/warepick/app/MainActivity.java
+++ android/app/src/main/java/com/warepick/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.warepick.app;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -10,5 +11,9 @@ public class MainActivity extends BridgeActivity {
         super.onCreate(savedInstanceState);
         // Enable Chrome DevTools remote debugging
         WebView.setWebContentsDebuggingEnabled(true);
+
+        WebView webView = getBridge().getWebView();
+        WebSettings settings = webView.getSettings();
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
     }
 }
